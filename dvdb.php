8140dfc0cb4724233b5485611001446776a263e4
funkmasterflow@gmx.de <funkmasterflow@gmx.de>
2011-06-30 00:45:14 +0200

    little bugfixes

--- dvdb.php
+++ dvdb.php
@@ -8,7 +8,7 @@ $movieList = '<table id="movieTable">';
 $movieList .= "<thead><tr><th>Titel</th><th>FSK</th><th>Dauer</th></tr></thead><tbody>";
 
 foreach ($movies as $movie){
-    $movieList .= "<tr><td>".$movie->col0."</td><td>".$movie->col1."</td><td>".$movie->col2."</td></tr>";
+        $movieList .= "<tr><td>".$movie->col0."</td><td>".$movie->col1."</td><td>".$movie->col2."</td></tr>";
 }
 
 $movieList .= "</tbody></table>";
